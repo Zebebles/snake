16d1572c0e4e4e86c4be55c9de73cc06120d07cf
Zebebles <zeb.muller@jbhifi.com.au>
2022-05-27 18:55:05 +1000

    Tile border colors

--- src/components/game/tile/Tile.styles.ts
+++ src/components/game/tile/Tile.styles.ts
@@ -10,7 +10,7 @@ export const styles: StylesList = {
     height: TILE_SIZE,
     width: TILE_SIZE,
     backgroundColor: "#6f886f",
-    border: `1px solid rgba(185, 237, 193, .2)`,
+    border: `2px solid rgba(185, 237, 193, .08)`,
 
     "& > img:not(.snakePart)": {
       width: TILE_SIZE - 10,
